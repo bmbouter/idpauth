f3df0c10c51af4d511867ffc123f36ccb26831bc
edhelms <baelout@gmail.com>
2010-08-05 14:48:57 -0400

    Fix minor bug.

--- context_preprocessors.py
+++ context_preprocessors.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from idpauth import authentication_tools
 
 def context_preprocessor(request):
+    d = {}
     institution = authentication_tools.get_institution(request)
     if institution != None:
         d['institution'] = institution
