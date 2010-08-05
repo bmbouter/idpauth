45a7997ea6595c84dd64c5dbdb00b49c77030a34
edhelms <baelout@gmail.com>
2010-08-05 14:35:18 -0400

    Removed opus_media_prefix reference.

--- context_preprocessors.py
+++ context_preprocessors.py
@@ -2,9 +2,6 @@ from django.conf import settings
 from idpauth import authentication_tools
 
 def context_preprocessor(request):
-    d = {
-        'opus_media_dir': settings.OPUS_MEDIA_PREFIX,
-    }
     institution = authentication_tools.get_institution(request)
     if institution != None:
         d['institution'] = institution
